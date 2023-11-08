dbffe73c2cb8a40849edeb37b004cb7777053da7
hee9841 <lee98417716@gmail.com>
2023-11-09 03:02:14 +0900

    feat: Collections.sort에서의 UnsupportedOperationException 버그 해결
    - unmodifiableList가 적용되어서 변경 행위가 금지되어서 발생
    
    Closes hee9841/java-lotto-6#1

--- src/main/java/lotto/domain/Seller.java
+++ src/main/java/lotto/domain/Seller.java
@@ -2,7 +2,7 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import constant.GameValues;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -15,16 +15,18 @@ public class Seller {
     }
 
     public List<Lotto> issueLotto(int ticketCount) {
-        return IntStream.rangeClosed(1, ticketCount)
-                .mapToObj(i -> new Lotto(generateLottoNumbers()))
+        return IntStream.rangeClosed(1, ticketCount).mapToObj(i -> new Lotto(generateLottoNumbers()))
                 .collect(Collectors.toList());
     }
 
     private List<Integer> generateLottoNumbers() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(
-                1, 45, 6);
-        Collections.sort(numbers);
-        return numbers;
+        return new ArrayList<>(
+                Randoms.pickUniqueNumbersInRange(
+                        GameValues.MIN_LOTTO_NUM.getValue(),
+                        GameValues.MAX_LOTTO_NUM.getValue(),
+                        GameValues.LOTTO_WINNING_NUM_SIZE.getValue()
+                )
+        );
     }
 
 }
